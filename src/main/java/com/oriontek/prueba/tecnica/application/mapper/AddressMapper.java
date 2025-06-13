448532e2e4d231e36eb5c15cb9ff7fc6ccabea9a
Isaias Acosta <acostalorensoi@gmail.com>
2025-06-13 07:30:58 -0400

    Add CustomerId = Id

--- src/main/java/com/oriontek/prueba/tecnica/application/mapper/AddressMapper.java
+++ src/main/java/com/oriontek/prueba/tecnica/application/mapper/AddressMapper.java
@@ -2,11 +2,10 @@ package com.oriontek.prueba.tecnica.application.mapper;
 
 import com.oriontek.prueba.tecnica.application.dto.AddressDTO;
 import com.oriontek.prueba.tecnica.domain.model.Address;
+import com.oriontek.prueba.tecnica.domain.model.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
-
-import com.oriontek.prueba.tecnica.domain.model.Customer;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper extends BaseMapper<Address, AddressDTO> {
@@ -15,11 +14,15 @@ public interface AddressMapper extends BaseMapper<Address, AddressDTO> {
     @Mapping(target = "customer", source = "customerId", qualifiedByName = "mapCustomer")
     Address toEntity(AddressDTO dto);
 
+    @Override
+    @Mapping(target = "customerId", source = "customer.id")
+    AddressDTO toDto(Address entity);
+
     @Named("mapCustomer")
     default Customer mapCustomer(Long id) {
         if (id == null) return null;
-        Customer c = new Customer();
-        c.setId(id);
-        return c;
+        Customer customer = new Customer();
+        customer.setId(id);
+        return customer;
     }
 }
